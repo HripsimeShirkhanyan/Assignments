4f79877c51a35d8b97c3e54408d336b6d1906fdd
HripsimeShirkhanyan <hshirkhanyan@gmail.com>
2018-06-30 02:13:36 +0400

    final lesson

---
+++ simple/simple/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //    simpnum();
+            //    Console.WriteLine();
+            //}
+
+
+            //static int simpnum()
+            //{
+            //    int number = int.Parse(Console.ReadLine());
+            //    int count = 0;
+            //    for (int i = 1; i <= number; i++)
+            //    {
+            //        if (number % i == 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //        if (count == 2)
+            //        {
+            //            Console.WriteLine("Simple");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Complex");
+
+            //        }
+
+            //    return number;
+            //}
+            //{
+            //    lool("a");
+
+            //}
+            //static int lool(string str)
+            //{
+            //    int a = Convert.ToInt32(Console.ReadLine());
+            //    if (a % 2 == 0)
+            //    {
+            //        Console.WriteLine("true");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("false");
+            //    }
+            //    return a;
+            //}
+
+
+
+            //    int[] a = { 1, 2, 3, 4, 5 };
+
+            //    int[] c = { 7, 8, 10, 9, 4 };
+
+            //    int a1 = maxValue(a);
+            //    int c1 = maxValue(c);
+            //    Console.WriteLine($"Max value of the first array is {a1}, second array {c1}");
+
+            //    int a2 = minValue(a);
+            //    int c2 = minValue(c);
+            //    Console.WriteLine($"Min value of the first array is {a2}, second array {c2}");
+
+            //}
+            //static int maxValue(int[] array)
+            //{
+
+            //    int max = array[0];
+            //    for (int i = 0; i < array.Length; i++)
+            //    {
+            //        if (array[i] > max)
+            //        {
+            //            max = array[i];
+            //        }
+
+            //    }
+            //    return max;
+            //}
+
+            //static int minValue (int[] array)
+            //{
+            //    int min = array[0];
+            //    for (int i = 0; i < array.Length; i++)
+            //        if (array[i] < min)
+            //        {
+            //            min = array[i];
+            //        }
+
+            //    return min;
+        //    int[] n = { 1, 5, 4, 8, 3, 9, 11, 7, 15, 2, 16};
+           
+        //    int a = maxValue(n);
+        //    Console.WriteLine(a);
+
+        //}
+
+        //static int maxValue (int[] array)
+        //{
+        //    int max = array[0];
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        if (array[i] % 2 == 1 && array[i] > max)
+        //        {
+        //            max = array[i];
+        //        }
+        //    }
+        //    return max;
+        //}
+    }
+}
